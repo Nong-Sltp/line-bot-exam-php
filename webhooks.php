9e8ce481b6e98ddb888cd20cb682a1e219626bb5
N <40230051+Nong-Sltp@users.noreply.github.com>
2022-02-11 22:29:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'HQ5RZTAw7hHdRrZz1SHv+Mzunv6CuLMHx4qWUhsywMnV40fWmKrEKiYT06dADnbZfEsJWsbVPrrQcaq0nRuSDeFYJ/YDfoQf3WWjqOPcAx9WfYLNsaB4EDiAEO56m7gtXYjplonRheSGThqFihEBVAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
